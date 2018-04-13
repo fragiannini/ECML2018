200b0423be0cc3e1639e58c66aa61c09980d451e
Francesco Giannini <fgiannini@diism.unisi.it>
2018-04-13 13:55:58 +0200

    Add files via upload

--- ex2.m
+++ ex2.m
@@ -1,93 +1,84 @@
-%supp sia tot sup, cioè quando so una sup per un pred la so per tutti gli
-%altri
+% This script produces the solution of Example 2 in the paper. We make use 
+% of the MATLAB function 'quadprog' that is specific for quadratic 
+% programming problems and provides as additional output a vector of 
+% Lagrange multipliers satisfying the KKT-conditions for the given solution
 
-%% PARAMETERS
+%% INITIALIZATION
 
-load('data3.mat')
-%examples
+% Supervisions and a domain 'dom' for the evaluation are loaded. The matrix 'L'
+% contains the points whose labels (for every predicate) are collected in
+% the matrix 'target'
+load('data2.mat')  
 
-global dom valDom S
 
-
-U=[];
+U=[]; % This set contains the unsupervised sample
 
 l=size(L,2);
 u=size(U,2);
+sdom=size(dom,2);
 
-%target=round(rand(3,l));
-%target=ones(3,l); %sono da metterci le vere sup per ogni predicato
-
-%U=round(rand(2,l)*10)*1/10;
-S=[L,U]; %SUPPONENDO L e U DISGIUNTI
-%S=unique([L,U]',t'rows','stable')';
+S=[L,U]; % This set contains the overall sample for the predicates
 s=size(S,2);
 
+print=1; % If this parameter is equal to 1 then the optimal solutions are plotted
+J=3; % Number of learnable predicates
+v=3; % Number of considered constrains
+eta=0.00001; % To avoid numerical issues
 
-stampa=0;
-t=3;
-v=3;
-eta=0.00001;
-
-
+% The following parameters define a common kernel for the predicates and we
+% use the script calckernel to get the Gram matrix on the sets 'dom' and
+% 'S'
 degree=2;
-sigma=1;
 
 options.Kernel='poly';
 options.KernelParam=degree;
-
-% options.Kernel='rbf';
-% options.KernelParam=sigma;
-
 
 K=calckernel(options,S');
 
 Kdom=calckernel(options,dom',S');
 
-%% INITIALIZZATIONS
-
+%% PARAMETERS FOR QUADPROG
 
 lb=[];
 ub=[];
 
-H=zeros(t*s,t*s);
+H=zeros(J*s,J*s);
 c=0;
-for i=1:t
+for i=1:J
     H(1+c:s+c,1+c:s+c)=K;
     c=c+s;
 end
 
-f=zeros(t*s,1);
+f=zeros(J*s,1);
 
 A=zeros();
 b=zeros();
 
-%A=zeros(t*l+2*t*s+v*s,t*s);
-%b=zeros(t*l+2*t*s+v*s,1);
-
-%% P-CONSTRAINTS
+%% POINTWISE CONSTRAINTS
+% These constraints enforce the available supervisions
 
 c=0;
-for i=1:t
+for i=1:J
     A(1+c:c+l,1+(i-1)*s:i*s)=-2*repmat(target(i,:)',1,s).*K(1:l,:);
     b(1+c:c+l,1)=-1-target(i,:)';
     c=c+l;
 end
 
-%% C-CONSTRAINTS
+%% CONSISTENCY CONSTRAINTS
+% These constraints enforce the predicate to be limited between 0 and 1
 
-if 1
-  for i=1:t
+  for i=1:J
       A(1+c:s+c,1+(i-1)*s:i*s)=K;
       A(1+s+c:2*s+c,1+(i-1)*s:i*s)=-K;
       b(1+c:c+s,1)=1;
       b(1+s+c:2*s+c,1)=0;
       c=c+2*s;
   end
-end
 
-%% L-CONSTRAINTS
+%% LOGICAL CONSTRAINTS
+% These are the linear constraints corresponding to the logical formulas
+% p_1(x)-> p_2(x), p_2(x)-> p_3(x), p_1(x)-> p_3(x)
 
-if 1
     A(c+1:c+s,:)=[K,-K,zeros(s,s)]; %1
     b(c+1:c+s,1)=zeros(s,1);
     c=c+s;
@@ -97,31 +88,27 @@ if 1
     A(c+1:c+s,:)=[K,zeros(s,s),-K]; %1
     b(c+1:c+s,1)=zeros(s,1);
     c=c+s;
-end
     
-%% OPTIM + VAL
-
+%% OPTIMIZATION AND EVALUATION
 
 
 [x,fval,exitflag,output,lambda] = quadprog(H+eye(size(H,1))*eta,f,A,b,[],[],lb,ub);
 
 
-valDom=zeros(t,sizedom);
+% Evaluation of the predicates on the domain
+valDom=zeros(J,sdom);
 
-%valutazione sui punti di DOM
 c=0;
-for i=1:t
+for i=1:J
     valDom(i,:)=x(1+c:s+c)'*Kdom;
     c=c+s;
 end
 
 %% PRINT 
 
-if stampa==1
+if print==1
 
 figure
-
-%legend({'NoLog','LogConv','LogNonConv'},'FontSize',12,'FontWeight','bold')
 
 grand=100;
 ax1 = subplot(3,1,1);
@@ -131,13 +118,9 @@ colorbar
 xlabel('p_1','FontSize',12)
 caxis([0,1])
 
-%title('Best Objective Functions \it','FontSize',16,'FontWeight','bold')
-
-
-% hold on
-% scatter(S(1,1),S(2,1),grand,'black','s','filled')
 hold on
-scatter(S(1,1:size(S,2)),S(2,1:size(S,2)),grand,'black','s','filled')
+scatter(S(1,1:s),S(2,1:s),grand,'black','s','filled')
+
 
 ax2 = subplot(3,1,2);
 scatter(dom(1,:),dom(2,:),grand,valDom(2,:),'s','filled')
@@ -146,10 +129,9 @@ colorbar
 xlabel('p_2','FontSize',12)
 caxis([0,1])
 
-% hold on
-% scatter(S(1,1),S(2,1),grand,'black','s','filled')
 hold on
-scatter(S(1,1:size(S,2)),S(2,1:size(S,2)),grand,'black','s','filled')
+scatter(S(1,1:s),S(2,1:s),grand,'black','s','filled')
+
 
 ax3 = subplot(3,1,3);
 scatter(dom(1,:),dom(2,:),grand,valDom(3,:),'s','filled')
@@ -158,11 +140,8 @@ colorbar
 xlabel('p_3','FontSize',12)
 caxis([0,1])
 
-% hold on
-% scatter(S(1,1:1),S(2,1:1),grand,'black','s','filled')
 hold on
-scatter(S(1,1:size(S,2)),S(2,1:size(S,2)),grand,'black','s','filled')
-
+scatter(S(1,1:s),S(2,1:s),grand,'black','s','filled')
 
 axis([0, 1, 0, 1])
 end
